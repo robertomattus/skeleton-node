be8a7cab500acd6e70c38599b15bb719508e23cd
robertomattus <robertmatt113@gmail.com>
2023-02-23 22:52:23 -0600

    Node basic skeleton created

---
+++ config.js
@@ -0,0 +1,57 @@
+
+const configs = {
+    api: {
+        port: process.env.PORT || 3000,
+        host: process.env.HOST || 'http://localhost:3000',
+        nodeEnv: process.env.NODE_ENV || 'development'
+    },
+    db: {
+        development: {
+            dialect: process.env.DB_DIALECT,
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            define: {
+                timestamps: true, 
+                underscored: true,
+                underscoredAll: true 
+            }
+        },
+        production: {
+            dialect: process.env.DB_DIALECT,
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            define: {
+                timestamps: true, 
+                underscored: true,
+                underscoredAll: true 
+            },
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            }
+        },
+        testing: {
+            dialect: process.env.DB_DIALECT,
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            define: {
+                timestamps: true, 
+                underscored: true,
+                underscoredAll: true 
+            }
+        }
+    }
+}
+
+module.exports = configs
