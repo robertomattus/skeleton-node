5284f4e8e5f83cb725df223d2dbd77c5fbcf5930
robertomattus <robertmatt113@gmail.com>
2023-02-24 20:12:47 -0600

    feature: protect api.js, auth.middleware and auth.services with .env variables

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 
 const configs = {
     api: {
         port: process.env.PORT || 3000,
         host: process.env.HOST || 'http://localhost:3000',
-        nodeEnv: process.env.NODE_ENV || 'development'
+        nodeEnv: process.env.NODE_ENV || 'development',
+        secretOrKey: process.env.JWT_SECRET
     },
     db: {
         development: {
--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const responseHandlers = require('./utils/handleResponses')
 const db = require('./utils/database')
 const initModels = require('./models/initModels')
+const config = require('../config').api
 
 const userRouter = require('./users/users.router')
 const authRouter = require('./auth/auth.router')
@@ -27,7 +28,7 @@ app.get('/', (req, res) => {
         status: 200,
         message: 'Servidor inicializado correctamente',
         data: {
-            "users": "http://localhost:9000/api/v1/users",
+            "users": `${config.host}/api/v1/users`,
         }
     })
 })
@@ -39,10 +40,10 @@ app.use('*', (req, res)=> {
     responseHandlers.error({
         res,
         status: 404,
-        message: 'URL not found, please try with http://localhost:9000/',
+        message: `URL not found, please try with ${config.host}`,
     })
 })
 
-app.listen(9000,() => {
-    console.log('Server started at port 9000')
+app.listen(config.port,() => {
+    console.log(`Server started at port ${config.port}`)
 })
